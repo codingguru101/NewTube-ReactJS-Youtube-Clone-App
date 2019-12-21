6f718f1034bbdce30d84b578caf88555cacd2d88
Alex Johnson <30718013+codingguru101@users.noreply.github.com>
2019-12-21 04:08:24 -0500

    added-videolist-component

--- src/App.js
+++ src/App.js
@@ -5,44 +5,41 @@ import { SearchBar, VideoList, VideoDetail } from "./components";
 
 import youtube from "./api/youtube";
 
-class App extends React.Component {
-  state = {
-    video: [],
-    selectedVideo: null
-  };
-  handleSubmit = async searchTerm => {
-    const response = await youtube.get("search", {
+export default () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  return (
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos }
+    } = await youtube.get("search", {
       params: {
         part: "snippet",
-        MaxResults: 5,
+        maxResults: 5,
         key: "AIzaSyBpAMNw6sGyRHafM3bSszeg7mpZxEqmWYc",
         q: searchTerm
       }
     });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    });
-  };
 
-  render() {
-    const { selectedVideo } = this.state;
-    return (
-      <Grid style={{ justifyContent: "center" }} container spacing={10}>
-        <Grid item xs={11}>
-          <Grid container spacing={10}>
-            <Grid item xs={12}>
-              <SearchBar onFormSubmit={this.handleSubmit} />
-            </Grid>
-            <Grid item xs={8}>
-              <VideoDetail video={selectedVideo} />
-            </Grid>
-            <Grid item xs={4} />
-          </Grid>
-        </Grid>
-      </Grid>
-    );
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
   }
-}
-
-export default App;
+};
